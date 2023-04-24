bc3cf61e24a40fc98f485397ecde02702bb8f0f5
DeaRick35 <131626220+DeaRick35@users.noreply.github.com>
2023-04-24 09:21:56 +0800

    create a game

--- js/main.js
+++ js/main.js
@@ -1,8 +1,74 @@
-window.onload = () => {
-  'use strict';
+// Set up variables
+var score = 0;
+var time = 30;
+var timerInterval;
+var ball = document.getElementById("ball");
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
+// Start the game
+startGame();
+
+function startGame() {
+  // Reset score and time
+  score = 0;
+  document.getElementById("score").innerHTML = score;
+  time = 30;
+  document.getElementById("time").innerHTML = time;
+
+  // Start the timer
+  timerInterval = setInterval(updateTimer, 1000);
+
+  // Add event listener to ball
+  ball.addEventListener("click", handleClick);
+
+  // Show the ball
+  showBall();
+}
+
+function showBall() {
+  // Generate random position for ball
+  var x = Math.floor(Math.random() * (window.innerWidth - 50));
+  var y = Math.floor(Math.random() * (window.innerHeight - 50));
+  ball.style.left = x + "px";
+  ball.style.top = y + "px";
+
+  // Show the ball
+  ball.style.display = "block";
+}
+
+function handleClick() {
+  // Increase score and update score display
+  score++;
+  document.getElementById("score").innerHTML = score;
+
+  // Hide the ball
+  ball.style.display = "none";
+
+  // Show the ball in a new position
+  showBall();
+}
+
+function updateTimer() {
+  // Decrease time and update time display
+  time--;
+  document.getElementById("time").innerHTML = time;
+
+  // End the game if time is up
+  if (time === 0) {
+    endGame();
   }
 }
+
+function endGame() {
+  // Stop the timer
+  clearInterval(timerInterval);
+
+  // Remove event listener from ball
+  ball.removeEventListener("click", handleClick);
+
+  // Hide the ball
+  ball.style.display = "none";
+
+  // Display final score
+  alert("Game over! Your score is " + score);
+}
+
